a443bf508e09a64b951b35098ebe8653c4c3d173
xqzh <xqzh@ideabinder.com>
2016-11-05 22:13:03 +0800

    Initial Commit

---
+++ AppleTV_P/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AppleTV_P
+//
+//  Created by xqzh on 16/11/5.
+//  Copyright © 2016年 xqzh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AppleTV_P/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  AppleTV_P
+//
+//  Created by xqzh on 16/11/5.
+//  Copyright © 2016年 xqzh. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
+}
+
+
+@end
